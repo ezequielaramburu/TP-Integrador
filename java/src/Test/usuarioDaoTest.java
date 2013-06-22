2c94638fb82ccaeaf0de5738cbb696de5a1b533d
Ezequiel Aramburu <ezequi8620@gmail.com>
2013-06-22 14:22:25 -0300

    Se agrega test

--- java/src/Test/usuarioDaoTest.java
+++ java/src/Test/usuarioDaoTest.java
@@ -10,6 +10,51 @@ public class usuarioDaoTest {
 
 	Usuario cosmeFulanito;
 	Usuario chuckNorris;
+	
+	@Before
+	public void setUp() throws PersistenceException {
+		// se borran todas las personas, para iniciar con base vacía
+		for (Usuario cadaPersona : dao.findAll()) {
+			dao.delete(cadaPersona);
+		}
+
+		// se inserta a Cosme Fulanito
+		cosmeFulanito = buildUsuario("Cosme", "Cosme", "Fulanito", "Cosme");
+		dao.insert(cosmeFulanito);
+
+		// Chuck Norris decide insertarse
+		chuckNorris = buildUsuario("Chuck", "Chuck", "Norris", "Chuck");
+		dao.insert(chuckNorris);
+	}
+	
+	private Usuario buildUsuario(String IdUsuario, String contrasenia, String nombre, String apellido) {
+		Persona usuario = new Usuario();
+		persona.setId(IdUsuario);
+		persona.setEdad(contrasenia);
+		persona.setNombre(nombre);
+		persona.setApellido(apellido);
+		
+		return usuario;
+	}
+	
+	@After
+	public void tearDown() throws PersistenceException {
+		// se borran todas las personas
+
+		dao.delete(cosmeFulanito);
+		dao.delete(chuckNorris);
+
+	}
+	
+	@Test
+	public void testQueSePuedeBuscarUnaPersona() throws PersistenceException {
+
+		Persona personaEncontrada = dao.findById(cosmeFulanito.getId());
+
+		assertNotNull("la persona con id 1 debe existir", personaEncontrada);
+		assertEquals("la persona 1 tiene nombre: Cosme", "Cosme", personaEncontrada.getApellido());
+		assertEquals("la persona 1 tiene apellido: Fulanito", "Fulanito", personaEncontrada.getNombre());
+		
 
 	
 }
